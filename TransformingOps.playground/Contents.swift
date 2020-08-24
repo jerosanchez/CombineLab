03aebfd2a6a910af4159789dada93ade70ec20e5
Jero Sanchez <jero.sanchez@icloud.com>
2020-08-24 18:41:14 +0200

    Improve comments in flatMap example

--- TransformingOps.playground/Contents.swift
+++ TransformingOps.playground/Contents.swift
@@ -59,14 +59,17 @@ example(of: "tryMap", action: {
 })
 
 example(of: "flatMap", action: {
+    // flatMap operator merges ("flattens") values coming from
+    // different streams into the same stream
+    
     let charlotte = Chatter(name: "Charlotte", message: "Hi, I'm Charlotte!")
     let james = Chatter(name: "James", message: "Hi, I'm James!")
     
     let chat = CurrentValueSubject<Chatter, Never>(charlotte)
     
     chat
-        // To keep memory footprint low, we can set the max number of
-        // publishers that can be "flatten" in the same stream;
+        // To keep memory footprint low, we set the max number
+        // of publishers that can be "flatten" in the same stream;
         // default maxPublishers value in .unlimited
         .flatMap(maxPublishers: .max(2))
             { $0.message }
@@ -77,15 +80,17 @@ example(of: "flatMap", action: {
     
     // We add a second publisher to the stream, i.e.
     // values to both charlotte.message and james.message are now
-    // merged ("flaten") in the same value stream
+    // merged ("flaten") in the same value stream;
+    
+    // note how we add a new stream by just re-assigning
     chat.value = james
     
     james.message.value = "James: Doing great. You?"
     charlotte.message.value = "Charlotte: I'm doing fine, thanks."
     
     let morgan = Chatter(name: "Morgan", message: "Hey guys, what are you up to?")
-    chat.value = morgan  // try to join conversation
+    chat.value = morgan  // Morgan tries to join the conversation
     
-    // Since maxPublishers is 2, Morgan message is never displayed
+    // Since maxPublishers is 2, Morgan messages are ignored
     charlotte.message.value = "Charlotte: Did you hear something?"
 })

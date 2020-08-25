53139b51bbc0dd5be3a4d9ddfd2f6573bd3dc72f
Jero Sanchez <jero.sanchez@icloud.com>
2020-08-25 16:33:16 +0200

    Add example of removeDuplicates() operator

--- FilteringOps.playground/Contents.swift
+++ FilteringOps.playground/Contents.swift
@@ -9,12 +9,26 @@ var subscriptions = Set<AnyCancellable>()
 // they just complete the stream with the error
 
 example(of: "filter", action: {
-    let publisher = (1...10).publisher
+    let numbers = (1...10).publisher
     
-    publisher
+    numbers
         .filter({ $0.isMultiple(of: 3) })
         
         .collect()
         .sink(receiveValue: { print( "Received values: ", $0) })
+        .store(in: &subscriptions)
+})
+
+example(of: "removeDuplicates", action: {
+    let words = "hey hey there! want to listen to mister mister ?"
+        .components(separatedBy: " ")
+        .publisher
+    
+    words
+        .removeDuplicates()
+        
+        .collect()
+        .map { $0.joined(separator: " ") }
+        .sink(receiveValue: { print($0) })
         .store(in: &subscriptions)
 })
